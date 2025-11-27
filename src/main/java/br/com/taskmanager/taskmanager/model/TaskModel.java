67a5224bdaf5620005f1c4933d3233c333c87e21
Eduardo Noda <duba.noda737@gmail.com>
2025-11-27 00:11:08 -0400

    feat: implementado modelos User, Task e Role

---
+++ src/main/java/br/com/taskmanager/taskmanager/model/TaskModel.java
@@ -0,0 +1,56 @@
+package br.com.taskmanager.taskmanager.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Data
+@Entity
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id")
+    private Long id;
+    @NotEmpty(message = "{task.title.not.empty}")
+    private String title;
+    @NotEmpty(message = "{task.description.not.empty}")
+    @Column(length = 1200)
+    @Size(max = 1200, message = "{task.description.size}")
+    private String description;
+    @NotNull(message = "{task.date.not.null}")
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private LocalDate date;
+    private boolean isCompleted;
+    private String creatorName;
+
+    public TaskModel(@NotEmpty String title,
+                     @NotEmpty @Size String description,
+                     @NotNull LocalDate date,
+                     boolean isCompleted,
+                     String creatorName) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.isCompleted = isCompleted;
+        this.creatorName = creatorName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        TaskModel taskModel = (TaskModel) o;
+        return isCompleted == taskModel.isCompleted && Objects.equals(id, taskModel.id) && Objects.equals(title, taskModel.title) && Objects.equals(description, taskModel.description) && Objects.equals(date, taskModel.date) && Objects.equals(creatorName, taskModel.creatorName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, date, isCompleted, creatorName);
+    }
+}
--- src/main/java/br/com/taskmanager/taskmanager/model/UserModel.java
+++ src/main/java/br/com/taskmanager/taskmanager/model/UserModel.java
@@ -1,16 +1,65 @@
 package br.com.taskmanager.taskmanager.model;
+
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@Setter
 @Entity
-@Table(name = "users")
-@Data
 public class UserModel {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
-    private String username;
-    private String cpf;
-    private RoleModel role;
 
+    @NotEmpty(message = "{user.username.not.empty}")
+    private String username;
+
+    @Email(message = "{user.email.not.valid}")
+    @NotEmpty(message = "{user.email.not.empty}")
+    @Column(unique = true)
+    private String email;
+
+    @NotEmpty(message = "{user.password.not.empty}")
+    @Length(min = 5, message = "{user.password.length}")
+    private String password;
+
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<RoleModel> role;
+
+    public UserModel() {
+    }
+
+    public UserModel(@NotEmpty String username,
+                     @Email @NotEmpty String email,
+                     @NotEmpty @Length(min = 5) String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        UserModel userModel = (UserModel) o;
+        return Objects.equals(id, userModel.id) && Objects.equals(username, userModel.username) && Objects.equals(email, userModel.email) && Objects.equals(password, userModel.password) && Objects.equals(role, userModel.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, email, password, role);
+    }
 }
