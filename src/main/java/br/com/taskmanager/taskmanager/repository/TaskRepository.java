39c4b93cf199cf451626d20bfd186d4e9defd40f
Eduardo Noda <duba.noda737@gmail.com>
2025-11-29 18:15:17 -0400

    fix: por ser necessário tirar uma função obsoleta, foi alterado o método de busca das tarefas

--- src/main/java/br/com/taskmanager/taskmanager/repository/TaskRepository.java
+++ src/main/java/br/com/taskmanager/taskmanager/repository/TaskRepository.java
@@ -4,9 +4,7 @@ import br.com.taskmanager.taskmanager.model.TaskModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface TaskRepository extends JpaRepository<TaskModel, Long> {
-    List<TaskModel> findByOwnerIdOrderByDueDateDesc(Long ownerId);
+    TaskModel findById(TaskModel task);
 }
